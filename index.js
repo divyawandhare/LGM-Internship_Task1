865dbab85547d1776eac0a912a424c5a1413f2ac
divyawandhare <68555772+divyawandhare@users.noreply.github.com>
2021-12-21 16:42:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function showSlides() {
   }
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
-  setTimeout(showSlides,2000)
+  setTimeout(showSlides,20000)
 }
 
 var slideIndex = 1;
